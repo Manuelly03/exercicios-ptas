a9a19c2290284e2da81761bab3dc3e581d622e60
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-24 03:06:09 -0300

    edit; adds a GET /tasks route with an in-memory array, ex2

--- app.js
+++ app.js
@@ -1,60 +1,14 @@
 import express from 'express';
 const app = express();
 
-app.use(express.json());
 const tarefas = [
   { id: 1, titulo: 'Estudar Express', concluida: false },
   { id: 2, titulo: 'Estudar JavaScript', concluida: true },
   { id: 3, titulo: 'Testar rotas', concluida: false }
 ];
-app.get('/', (req, res) => {
-  res.send('API está funcionando!');
-});
+
 app.get('/tarefas', (req, res) => {
-  const { concluida } = req.query; 
-  if (concluida !== undefined) {
-    const statusBuscado = concluida === 'true';
-    const tarefasFiltradas = tarefas.filter(t => t.concluida === statusBuscado);
-    return res.json(tarefasFiltradas); 
-  }
-
-  res.json(tarefas); 
-});
-app.get('/tarefas/:id', (req, res) => {
-    const idBuscado = parseInt(req.params.id);
-    const tarefaEncontrada = tarefas.find(t => t.id === idBuscado);
-
-  if (!tarefaEncontrada) {
-    return res.status(404).json({ erro: 'Tarefa não encontrada' });
-  }
-  res.json(tarefaEncontrada);
-});
-function autenticacao(req, res, next) {
-  console.log('-> [Middleware 1]: Verificando autenticação...');
-  next(); 
-}
-function validarCorpo(req, res, next) {
-  console.log('-> [Middleware 2]: Validando dados enviadoss...');
-  const { titulo } = req.body;
-
-  if (!titulo || titulo.trim() === '') {
-    return res.status(400).json({ erro: 'O campo "titulo" é obrigatório' });
-  }
-  next();
-}
-function logger(req, res, next) {
-  console.log('-> [Middleware 3]: Registro de log da ação...');
-  next();
-}
-app.post('/tarefas', [autenticacao, validarCorpo, logger], (req, res) => {
-  const { titulo } = req.body;
-  const novaTarefa = {
-    id: tarefas.length + 1,
-    titulo: titulo,
-    concluida: false
-};
-  tarefas.push(novaTarefa);
-  res.status(201).json(novaTarefa);
+  res.json(tarefas);
 });
 
 app.listen(3000, () => {
